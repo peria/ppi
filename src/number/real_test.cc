65281abc4a5345c2769b5ad74f9ab4a38617291c
Hitoshi Yoshida <github@peria.jp>
2015-10-31 18:04:15 +0900

    Enable test

--- src/number/real_test.cc
+++ src/number/real_test.cc
@@ -8,10 +8,14 @@ namespace ppi {
 namespace number {
 
 // Segmentation fault, some Real instances have no memory for mantissa_.
-TEST(RealTest, DISABLED_InverseSqrt) {
+TEST(RealTest, InverseSqrt) {
   Real val;
   Real::InverseSqrt(2, &val);
-  LOG(INFO) << val;
+  std::ostringstream oss;
+  oss << val;
+
+  const std::string expect = "0.b504f333f9d";
+  EXPECT_EQ(expect, oss.str().substr(0, expect.size()));
 }
 
 }  // namespace number
