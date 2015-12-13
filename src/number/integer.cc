506650e1a2f8054ebb27742edec773f4e34e8f53
Hitoshi Yoshida <github@peria.jp>
2015-12-14 01:16:22 +0900

    Implement IntegerPiece.

--- src/number/integer.cc
+++ src/number/integer.cc
@@ -7,22 +7,10 @@
 
 #include "base/base.h"
 #include "fmt/fmt.h"
+#include "number/integer_piece.h"
 
 namespace ppi {
 namespace number {
-
-namespace {
-
-// kMaxComplexsForMult must be equal to kMaxSize in fft.cc.
-const int64 kMaxLimbsForMult = 1 << 17;
-const int64 kMaxShortLimbsForMult = kMaxLimbsForMult * 4;
-const int64 kMaxComplexsForMult = kMaxShortLimbsForMult * 2;
-double g_workarea[2][kMaxComplexsForMult];
-
-const int kMaskBitSize = 16;
-const uint64 kMask = (1ULL << kMaskBitSize) - 1;
-
-}  // namespace
 
 Integer::Integer() : std::vector<uint64>() {}
 
@@ -42,49 +30,20 @@ void Integer::Normalize() {
 
 // static
 void Integer::Add(const Integer& a, const Integer& b, Integer* c) {
-  const int64 n = std::min(a.ssize(), b.ssize());
-  c->resize(std::max(a.ssize(), b.ssize()));
-
-  uint64 carry = 0;
-  int64 i = 0;
-  for (i = 0; i < n; ++i) {
-    uint64 s = b[i] + carry;
-    carry = (s < b[i]) ? 1 : 0;
-    (*c)[i] = a[i] + s;
-    carry += ((*c)[i] < s) ? 1 : 0;
-  }
-  for (; i < a.ssize(); ++i) {
-    (*c)[i] = a[i] + carry;
-    carry = ((*c)[i] < carry) ? 1 : 0;
-  }
-  for (; i < b.ssize(); ++i) {
-    (*c)[i] = b[i] + carry;
-    carry = ((*c)[i] < carry) ? 1 : 0;
-  }
+  c->resize(std::max(a.size(), b.size()));
+  IntegerPiece cp(*c);
+  uint64 carry = IntegerPiece::Add(IntegerPiece(a), IntegerPiece(b), &cp);
   if (carry) {
     c->push_back(carry);
   }
-
   c->Normalize();
 }
 
 // static
 void Integer::Subtract(const Integer& a, const Integer& b, Integer* c) {
-  c->resize(a.ssize());
-
-  uint64 carry = 0;
-  for (int64 i = 0; i < b.ssize(); ++i) {
-    uint64 s = a[i] - carry;
-    carry = (s > a[i]) ? 1 : 0;
-    (*c)[i] = s - b[i];
-    carry += ((*c)[i] > s) ? 1 : 0;
-  }
-  for (int64 i = b.ssize(); i < a.ssize(); ++i) {
-    uint64 s = a[i] - carry;
-    carry = (s > a[i]) ? 1 : 0;
-    (*c)[i] = s;
-  }
-
+  c->resize(a.size());
+  IntegerPiece cp(*c);
+  IntegerPiece::Subtract(IntegerPiece(a), IntegerPiece(b), &cp);
   c->Normalize();
 }
 
@@ -108,145 +67,27 @@ double Integer::Mult(const Integer& a, const Integer& b, Integer* c) {
   const int64 na = a.size();
   const int64 nb = b.size();
   const int64 n = MinPow2(na + nb) / 2;
-  CHECK_GE(kMaxLimbsForMult, n) << " from " << (na + nb);
-
-  double* da = g_workarea[0];
-  double* db = g_workarea[1];
-
-  // Split uint64[na] -> double[4n][2]
-  Split4In8(a, n, da);
-  // FMT it, with q=1/4
-  fmt::Fmt::Fmt4(fmt::Fft::Type::Forward, 4 * n, da);
-
-  if (&a == &b) {
-    db = da;
-  } else {
-    Split4In8(b, n, db);
-    fmt::Fmt::Fmt4(fmt::Fft::Type::Forward, 4 * n, db);
-  }
-
-  for (int64 i = 0; i < 4 * n; ++i) {
-    double ar = da[2 * i], ai = da[2 * i + 1];
-    double br = db[2 * i], bi = db[2 * i + 1];
-    da[2 * i] = ar * br - ai * bi;
-    da[2 * i + 1] = ar * bi + ai * br;
-  }
-  
-  fmt::Fmt::Fmt4(fmt::Fft::Type::Inverse, 4 * n, da);
-
-  // Gather double[8n][2] -> int[2n]
   c->resize(2 * n);
-  double err = Gather4In8(da, c);
+  IntegerPiece cp(*c);
+  double err = IntegerPiece::Mult(IntegerPiece(a), IntegerPiece(b), &cp);
   c->Normalize();
 
   return err;
 }
 
-double Integer::Gather4In8(double* da, Integer* a) {
-  DCHECK_EQ(0, a->size() % 2);
-  const int64 n = a->size() / 2;
-
-  double err = 0;
-  // 1. Dobule -> integral double
-  for (int64 i = 0; i < 8 * n; ++i) {
-    double dd = da[i];
-    da[i] = std::floor(dd + 0.5);
-    double e = std::abs(dd - da[i]);
-    err = std::max(e, err);
-  }
-
-  // 2. Normalize & re-alignment
-  uint64 carry = 0;
-  double* d = da;
-  for (int64 i = 0; i < n; ++i) {
-    uint64 ia0 = d[0], ia1 = d[2], ia2 = d[4], ia3 = d[6];
-    ia0 += carry;
-    ia1 += ia0 >> kMaskBitSize;
-    ia2 += ia1 >> kMaskBitSize;
-    ia3 += ia2 >> kMaskBitSize;
-    carry = ia3 >> kMaskBitSize;
-    ia0 &= kMask;
-    ia1 &= kMask;
-    ia2 &= kMask;
-    (*a)[i] = (((((ia3 << kMaskBitSize) + ia2) << kMaskBitSize) + ia1) << kMaskBitSize) + ia0;
-    d += 8;
-  }
-  d = da;
-  for (int64 i = 0; i < n; ++i) {
-    uint64 ia0 = d[1], ia1 = d[3], ia2 = d[5], ia3 = d[7];
-    ia0 += carry;
-    ia1 += ia0 >> kMaskBitSize;
-    ia2 += ia1 >> kMaskBitSize;
-    ia3 += ia2 >> kMaskBitSize;
-    carry = ia3 >> kMaskBitSize;
-    ia0 &= kMask;
-    ia1 &= kMask;
-    ia2 &= kMask;
-    (*a)[i + n] = (((((ia3 << kMaskBitSize) + ia2) << kMaskBitSize) + ia1) << kMaskBitSize) + ia0;
-    d += 8;
-  }
-
-  return err;
-}
-
-void Integer::Split4In8(const Integer& a, const int64 n, double* da) {
-  std::fill_n(da, n * 8, 0.0);
-
-  const int64 na = a.size();
-  for (int64 i = 0; i < std::min(n, na); ++i) {
-    uint64 ia = a[i];
-    da[8 * i] = ia & kMask;
-    da[8 * i + 2] = (ia >> kMaskBitSize) & kMask;
-    da[8 * i + 4] = (ia >> (kMaskBitSize * 2)) & kMask;
-    da[8 * i + 6] = ia >> (kMaskBitSize * 3);
-  }
-  for (int64 i = n, j = 0; i < na; ++i, ++j) {
-    uint64 ia = a[i];
-    da[8 * j + 1] = ia & kMask;
-    da[8 * j + 3] = (ia >> kMaskBitSize) & kMask;
-    da[8 * j + 5] = (ia >> (kMaskBitSize * 2)) & kMask;
-    da[8 * j + 7] = ia >> (kMaskBitSize * 3);
-  }
-}
-
-namespace {
-// TODO: Move these constant values to a header in base/.
-const int64 kHalfSize = 32;
-const int64 kHalfBitMask = (1ULL << kHalfSize) - 1;
-}
-
-// TODO: Accept uint64 as a second argument.
-void Integer::Mult(const Integer& a, const uint32 b, Integer* c) {
+void Integer::Mult(const Integer& a, const uint64 b, Integer* c) {
   c->resize(a.size());
-  uint64 carry = 0;
-  for (size_t i = 0; i < a.size(); ++i) {
-    uint64 a_low = a[i] & kHalfBitMask;
-    uint64 a_high = a[i] >> kHalfSize;
-    uint64 c_low = a_low * b + carry;
-    uint64 c_high = a_high * b;
-    (*c)[i] = c_low + (c_high << kHalfSize);
-    carry = c_high >> kHalfSize;
-    if ((*c)[i] < c_low)
-      ++carry;
-  }
+  IntegerPiece cp(*c);
+  uint64 carry = IntegerPiece::Mult(IntegerPiece(a), b, &cp);
   if (carry) {
     c->push_back(carry);
   }
 }
 
-void Integer::Div(const Integer& a, const uint32 b, Integer* c) {
-  uint64 limb = 0;
-  for (int64 i = a.size() - 1; i >= 0; --i) {
-    uint64 ia = a[i];
-    limb = (limb << kHalfSize) + (ia >> kHalfSize);
-    uint64 ic = limb / b;
-    limb %= b;
-    limb = (limb << kHalfSize) + (ia & kHalfBitMask);
-    ic = (ic << kHalfSize) + limb / b;
-    limb %= b;
-    (*c)[i] = ic;
-  }
+void Integer::Div(const Integer& a, const uint64 b, Integer* c) {
   c->resize(a.size());
+  IntegerPiece cp(*c);
+  IntegerPiece::Div(IntegerPiece(a), b, &cp);
   c->Normalize();
 }
 
@@ -257,11 +98,7 @@ Integer& Integer::operator=(uint64 a) {
 }
 
 std::ostream& operator<<(std::ostream& os, const Integer& val) {
-  static char buffer[50];
-  for (int64 i = val.size() - 1; i >= 0; --i) {
-    sprintf(buffer, "%016lx", val[i]);
-    os << buffer;
-  }
+  os << IntegerPiece(val);
   return os;
 }
 
--- src/number/integer_test.cc
+++ src/number/integer_test.cc
@@ -6,12 +6,6 @@
 namespace ppi {
 namespace number {
 
-class IntegerForTest : public Integer {
- public:
-  using Integer::Split4In8;
-  using Integer::Gather4In8;
-};
-
 TEST(IntegerTest, ErrorInMult) {
   const int n = 64;
   Integer a, b, c;
@@ -24,31 +18,6 @@ TEST(IntegerTest, ErrorInMult) {
 
   double err = Integer::Mult(a, b, &c);
   EXPECT_GT(1e-2, err);
-}
-
-TEST(IntegerTest, SplitAndGather) {
-  Integer a;
-  double d[4 * 2];
-  a.push_back(0x1234567890abcdefULL);
-
-  // Integer -> double[4][2]
-  IntegerForTest::Split4In8(a, 1, d);
-  EXPECT_EQ(static_cast<double>(0xcdef), d[0]);
-  EXPECT_EQ(0.0, d[1]);
-  EXPECT_EQ(static_cast<double>(0x90ab), d[2]);
-  EXPECT_EQ(0.0, d[3]);
-  EXPECT_EQ(static_cast<double>(0x5678), d[4]);
-  EXPECT_EQ(0.0, d[5]);
-  EXPECT_EQ(static_cast<double>(0x1234), d[6]);
-  EXPECT_EQ(0.0, d[7]);
-
-  // double[4][2] -> Integer
-  d[1] = 3141.0;
-  Integer b;
-  b.resize(2);
-  IntegerForTest::Gather4In8(d, &b);
-  EXPECT_EQ(0x1234567890abcdefULL, b[0]);
-  EXPECT_EQ(3141ULL, b[1]);
 }
 
 TEST(IntegerTest, Mult) {
