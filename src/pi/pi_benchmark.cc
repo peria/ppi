7ae3839f9331aa6982892c7760e7a9fb2bf2d284
Hitoshi Yoshida <github@peria.jp>
2015-12-26 22:00:56 +0900

    Make a benchmark for Chudnovsky

---
+++ src/pi/pi_benchmark.cc
@@ -0,0 +1,19 @@
+#include "pi/pi.h"
+
+#include <benchmark/benchmark.h>
+
+#include "base/base.h"
+#include "number/real.h"
+
+using ppi::int64;
+
+void BM_PiChudnovsky(benchmark::State& state) {
+  ppi::number::Real pi;
+  int64 limbs = state.range_x() / 16 + 1;
+  pi.setPrecision(limbs);
+  ppi::pi::Pi::Chudnovsky(&pi);
+}
+
+BENCHMARK(BM_PiChudnovsky)->Arg(100000)->Arg(1000000);
+
+BENCHMARK_MAIN();
