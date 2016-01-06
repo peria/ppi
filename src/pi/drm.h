f913bb6a56d09e4ae4a5bcecee1115eae1a9b79b
Hitoshi Yoshida <github@peria.jp>
2016-01-06 18:50:59 +0900

    add DRM files

---
+++ src/pi/drm.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "base/base.h"
+#include "number/real.h"
+
+namespace ppi {
+namespace pi {
+
+using number::Real;
+using number::Integer;
+
+class Drm {
+ public:
+  // Computes pi using arctan formula in O(n(log n)^3) algorithm.
+  // Returns the maximum rounding error in Multiplications.
+  static double Chudnovsky(Real* pi);
+
+ private:
+  static double Internal(int64 n0, int64 n1,
+                         Integer* a0, Integer* b0, Integer* c0);
+  static void SetValues(int64 n, Integer* a, Integer* b, Integer* c);
+};
+
+}  // namespace pi
+}  // namespace ppi
