cc4fcbcf9991c63553fd889481a91d3a00a5fc72
Hitoshi Yoshida <github@peria.jp>
2018-01-09 10:28:01 +0900

    Update travis.sh

--- build/travis.sh
+++ build/travis.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+if [ $1 ]; then
+  BUILD_TYPE=$1
+else
+  BUILD_TYPE=Debug
+fi
+
 rootdir="$(cd $(dirname "$0")/..; pwd)"
-outdir="$rootdir/out/Debug"
+outdir="$rootdir/out/${BUILD_TYPE}"
 srcdir="$rootdir/src"
 
 declare -a cmake_opts
 cmake_opts+=("-GNinja")
 cmake_opts+=("-DCMAKE_C_COMPILER=clang")
 cmake_opts+=("-DCMAKE_CXX_COMPILER=clang++")
-cmake_opts+=("-DCMAKE_BUILD_TYPE=Debug")
+cmake_opts+=("-DCMAKE_BUILD_TYPE=${BUILD_TYPE}")
 
 mkdir -p $outdir
 cd $outdir
